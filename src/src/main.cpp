8f0a5b60532a8d26f216925e75d4c08de8bd6af3
nahueltaibo <nahueltaibo@gmail.com>
2021-08-05 17:32:40 -0400

    loading scripts and styles from CDN instead of device, for reduced firmware size

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -143,14 +143,11 @@ void processClientRequest(WiFiClient client)
 
           // Display the HTML web page
           client.println("<!DOCTYPE html><html>");
-          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-          client.println("<link rel=\"icon\" href=\"data:,\">");
-          // CSS to style the on/off buttons
-          // Feel free to change the background-color and font-size attributes to fit your preferences
-          client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-          client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-          client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-          client.println(".button2 {background-color: #77878A;}</style></head>");
+          client.println("<head>");
+          client.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+          client.println("<link rel='icon' href='data:,'>");
+          client.println("<link href='https://cdn.jsdelivr.net/gh/nahueltaibo/irrigator@esp-01s/src/data/styles.css' rel='stylesheet'></link>");
+          client.println("</head>");
 
           // Web Page Heading
           client.println("<body><h1>ESP8266 Web Server</h1>");
@@ -160,14 +157,14 @@ void processClientRequest(WiFiClient client)
           // If the output5State is off, it displays the ON button
           if (relayState == "off")
           {
-            client.println("<p><a href=\"/relay/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href='/relay/on'><button class='button'>ON</button></a></p>");
           }
           else
           {
-            client.println("<p><a href=\"/relay/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href='/relay/off'><button class='button button2'>OFF</button></a></p>");
           }
 
-          client.println("</script></html>");
+          client.println("<script src='https://cdn.jsdelivr.net/gh/nahueltaibo/irrigator@esp-01s/src/data/main.js'></script>");
           client.println("</body></html>");
 
           // The HTTP response ends with another blank line
